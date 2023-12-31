433a708a794f5f13d583bfd685b89ad402be79b4
Hoang Minh Thanh <hmthanhgm@gmail.com>
2023-12-31 18:10:55 +0700

    init update

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next';
+// import { ConnectKitButton } from 'connectkit';
+
+const Home: NextPage = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      Hello
+    </div>
+  );
+};
+{/* <ConnectKitButton /> */}
+
+export default Home;
